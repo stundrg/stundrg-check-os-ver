206bfd4601498b1ca9a4a71d743d707f5872b2d2
stundrg <khr0106@naver.com>
2025-01-14 17:45:39 +0900

    test_first.py 주석 수정

--- tests/test_first.py
+++ tests/test_first.py
@@ -2,5 +2,11 @@ from stundrg_check_os_ver.osver import get_os_pretty_name
 
 def test_first():
     v = get_os_pretty_name()
+    assert v is not None
     assert v == "Ubuntu 24.04.1 LTS"
-
+    # 빈 문자열이 아닌지
+    # 문자열에 LTS가 포함 되었는지
+    # 문자열에 문자도 있고, 숫자도 있는지
+    # .이 포함되어 있는지
+    # 기타 등등 ...
+    # 길이가 적어도 얼마 이상인지 ............
