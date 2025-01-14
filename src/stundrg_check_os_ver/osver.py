d4fe91206c20765642d60b259433ebc722c43fd1
stundrg <khr0106@naver.com>
2025-01-14 17:20:03 +0900

    test 세팅 완료

--- src/stundrg_check_os_ver/osver.py
+++ src/stundrg_check_os_ver/osver.py
@@ -2,5 +2,8 @@ def get_os_pretty_name() -> str:
     with open('/etc/os-release', 'r') as f:
         for line in f:
             if line.startswith('PRETTY_NAME='):
-                return line.split('=')[1].replace('\n', '').strip('"')
+                # Ubuntu 24.04.1 LTS
+                r =  line.split('=')[1].replace('\n', '').strip('"')
+                print(r)
+                return r
     return None
